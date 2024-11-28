629d49f829a8ecbb0441d595289860df624f9118
FakeManiGH <timo.anjala@gmail.com>
2024-11-29 00:22:54 +0200

    export fix

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-export { default as ImageBrowser } from './imageBrowser.jsx';
+import ImageBrowser from './imageBrowser.jsx';
 
+// ES6 export
+export { ImageBrowser };
+
+// CommonJS export
 module.exports = {
-    ImageBrowser: require('./imageBrowser.jsx')
+    ImageBrowser: require('./imageBrowser.jsx').default
 };
