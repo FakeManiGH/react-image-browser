5b412f2e2355d9ff42f89a56ec7ac7c5ff8f35b4
FakeManiGH <timo.anjala@gmail.com>
2024-11-29 00:43:00 +0200

    index -file removed

--- index.js
+++
@@ -1,9 +0,0 @@
-import ImageBrowser from './imageBrowser.jsx';
-
-// ES6 export
-export { ImageBrowser };
-
-// CommonJS export
-module.exports = {
-    ImageBrowser: require('./imageBrowser.jsx').default
-};
